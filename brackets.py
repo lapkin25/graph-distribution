674aec8ee81091bc527e87d6026f3164414047ce
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2026-03-16 19:51:05 +1000

    Попытка вычислить дисперсию
    (возможна ошибка: результат слишком большой)

--- brackets.py
+++ brackets.py
@@ -1,15 +1,5 @@
 import networkx as nx
 
-"""
-class Brackets:
-    class BracketsItem:
-        def __init__(self):
-            self.node = None
-
-
-    def __init__(self, source):
-        self.source = source
-"""
 
 class Bracket:
     def __init__(self, node, brackets_list, adj_list):
@@ -26,10 +16,12 @@ class Bracket:
     def print(self, indentation=0):
         print(' ' * indentation, '[')
         print(' ' * indentation, f'Вершина {self.node}:')
+        print(' ' * indentation, f'Число развилок: {len(self.branches)}')
         for i in range(len(self.branches)):
             print(' ' * indentation, f'Ребро #{self.edges[i]} в {self.adj_nodes[i]}')
             self.branches[i].print(indentation + 1)
         print(' ' * indentation, ']')
+
 
 def backtracking(G, source, current, stack):
     if current == source:
@@ -52,7 +44,23 @@ def backtracking(G, source, current, stack):
         return br
 
 def get_bracket(G, source, destination):
-    return backtracking(G, source, destination, [])
+    return backtracking(G, source, destination, [destination])
+
+def tree_search(br, coefs, product):
+    print(f"Вершина {br.node}: {len(br.branches)} разветвлений: {br.edges}")
+    print(coefs)
+    if len(br.branches) == 0:
+        return 0.0
+    lam = 1.0 / len(br.branches)
+    for i in range(len(br.branches)):
+        coefs[br.edges[i]] += product * lam
+        tree_search(br.branches[i], coefs, product * lam)
+    return sum([x ** 2 for x in coefs])
+
+def calc_variance_uniformly(br):
+    coefs = [0.0] * len(G.edges)
+    return tree_search(br, coefs, 1.0)
+
 
 
 G = nx.Graph()
@@ -69,3 +77,5 @@ for i, e in enumerate(G.edges):
 
 br = get_bracket(G, 1, 6)
 br.print()
+print("Рёбра:", G.edges)
+print(calc_variance_uniformly(br))
