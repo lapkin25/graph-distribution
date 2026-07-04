83393670f1e81d2faa5389ff39cb5368259b6651
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2026-07-04 19:31:41 +1000

    Update model-1.py

--- model-1/model-1.py
+++ model-1/model-1.py
@@ -1,5 +1,7 @@
 import networkx as nx
 import numpy as np
+from scipy.optimize import Bounds, LinearConstraint, minimize
+
 
 
 G = nx.Graph()
@@ -11,6 +13,9 @@ source = 1
 destination = 4
 
 
+"""
+Начало раздела "функции"
+"""
 
 def get_initial_orientation(G):
     """
@@ -74,6 +79,7 @@ def calc_variance(orientation, alpha, start, finish):
           finish - конечная вершина
     Выход: variance - вектор дисперсий сигнала во всех вершинах
     """
+    print(alpha)
     # проверить корректность ориентировки
     if not is_orientation_correct(orientation, start, finish):
         raise
@@ -82,10 +88,10 @@ def calc_variance(orientation, alpha, start, finish):
 
     # Посчитать коэффициенты при шумах на всех рёбрах...
     # словарь "вершина -> массив коэффициентов при шумах на всех рёбрах"
-    beta = {start: np.zeros(len(orientation.nodes()))}
+    beta = {start: np.zeros(len(orientation.edges()))}
     # для каждой вершины...
     for u in topsort:
-        beta[u] = np.zeros(len(orientation.nodes()))
+        beta[u] = np.zeros(len(orientation.edges()))
         # перебрать все входящие рёбра
         for v, _, data in orientation.in_edges(u, data=True):
             # v - вершина, из которой идёт ребро в u
@@ -106,6 +112,23 @@ def calc_variance(orientation, alpha, start, finish):
     return variance
 
 
+def get_initial_alphas(orientation):
+    """
+    Вход: orientation - орграф (ориентировка)
+    Выход: alpha - начальное приближение для коэффициентов
+      (распределение поровну по рёбрам, входящим в каждую вершину)
+    """
+    num_alphas = len(orientation.edges())
+    alpha = np.zeros(num_alphas)
+    for u in orientation.nodes():
+        num_incoming = orientation.in_degree(u)
+        for v, _, data in orientation.in_edges(u, data=True):
+            # v - вершина, из которой идёт ребро в u
+            ind = data['num']  # индекс ребра (v, u)
+            alpha[ind] = 1. / num_incoming
+    return alpha
+
+
 def optimize_coefs(orientation, initial_alpha, start, finish):
     """
     Вход: orientation - орграф (ориентировка)
@@ -115,7 +138,37 @@ def optimize_coefs(orientation, initial_alpha, start, finish):
     Выход: optimal_alpha - оптимальный вектор коэффициентов, обеспечивающий
       минимум дисперсии сигнала в конечной вершине
     """
-    pass
+    # целевая функция для оптимизации
+    def objective_function(alpha):
+        return calc_variance(orientation, alpha, start, finish)[finish]
+
+    # число переменных - это число рёбер
+    len_alpha = len(initial_alpha)
+    # ограничения: каждая переменная от 0 до 1
+    bounds = Bounds([0] * len_alpha, [1] * len_alpha)
+    # линейные ограничения: сумма переменных по входящим рёбрам равна 1
+    # - заполняем матрицу A: строки - это ограничения, столбцы - это переменные
+    num_constraints = sum([orientation.in_degree(v) for v in orientation.nodes()])
+    A = np.zeros((num_constraints, len_alpha))
+    k = 0  # счётчик ограничений
+    for u in orientation.nodes():
+        if u != start:
+            # во всех вершинах, кроме стартовой, есть входящие рёбра
+            for v, _, data in orientation.in_edges(u, data=True):
+                # v - вершина, из которой идёт ребро в u
+                ind = data['num']  # индекс ребра (v, u)
+                A[k, ind] = 1.0
+    # - заполняем вектор правых частей ограничений
+    rhs = np.ones(num_constraints)
+    # - создаем линейное ограничение типа "равенство"
+    linear_constraint = LinearConstraint(A, rhs, rhs)
+    # решение задачи оптимизации
+    res = minimize(objective_function, initial_alpha, method='trust-constr',
+                   constraints=linear_constraint, bounds=bounds, options={'verbose': 1})
+    optimal_alpha = res.x
+
+    return optimal_alpha
+
 
 
 def optimize_orientation(graph, start, finish):
@@ -129,6 +182,10 @@ def optimize_orientation(graph, start, finish):
     pass
 
 
+"""
+Конец раздела "функции"
+"""
+
 
 # добавить служебную информацию к рёбрам графа
 # нумерация ребер графа
@@ -140,5 +197,29 @@ for i, e in enumerate(G.edges):
 
 orientation0 = get_initial_orientation(G)
 optimal_orientation = optimize_orientation(G, source, destination)
+alpha0 = get_initial_alphas(orientation0)
+print(optimize_coefs(orientation0, alpha0, 1, 4))
+#print(calc_variance(orientation0, None, 1, 4))
 
-print(calc_variance(orientation0, None, 1, 4))
+
+"""
+import matplotlib.pyplot as plt
+
+# Вычисляем позиции узлов (алгоритм spring_layout помогает избежать наложения)
+pos = nx.spring_layout(orientation0, seed=42)
+
+# Рисуем граф
+nx.draw(
+    orientation0,
+    pos,
+    with_labels=True,  # Показывать метки узлов
+    node_color="lightblue",  # Цвет узлов
+    node_size=1500,  # Размер узлов
+    font_size=12,  # Размер шрифта меток
+    font_weight="bold",  # Жирность шрифта
+    arrows=True  # Для DiGraph стрелки рисуются по умолчанию
+)
+
+# Показываем график
+plt.show()
+"""
