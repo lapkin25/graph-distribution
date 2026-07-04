0b77c95d17a3e378b5604311da13e3348375495b
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2026-07-04 16:49:37 +1000

    Update model-1.py

--- model-1/model-1.py
+++ model-1/model-1.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import numpy as np
+
 
 G = nx.Graph()
 
@@ -7,6 +9,7 @@ G.add_edges_from([(1, 2), (1, 3), (2, 4), (3, 4), (2, 3)])
 
 source = 1
 destination = 4
+
 
 
 def get_initial_orientation(G):
@@ -23,8 +26,8 @@ def get_initial_orientation(G):
     # создать пустой орграф
     G1 = nx.DiGraph()
     # добавить к орграфу ребра неориентированного графа
-    for u, v in G.edges():
-        G1.add_edge(u, v)
+    for u, v, data in G.edges(data=True):
+        G1.add_edge(u, v, num=data['num'])
     return G1
 
 
@@ -63,14 +66,44 @@ def is_orientation_correct(orientation, start, finish):
     return True
 
 
-def calc_variance(orientation, alpha, start):
+def calc_variance(orientation, alpha, start, finish):
     """
     Вход: orientation - орграф (ориентировка)
           alpha - вектор коэффициентов (на всех рёбрах)
           start - начальная вершина
+          finish - конечная вершина
     Выход: variance - вектор дисперсий сигнала во всех вершинах
     """
-    pass
+    # проверить корректность ориентировки
+    if not is_orientation_correct(orientation, start, finish):
+        raise
+    # определить порядок обработки вершин графа (топологическая сортировка)
+    topsort = list(nx.topological_sort(orientation))
+
+    # Посчитать коэффициенты при шумах на всех рёбрах...
+    # словарь "вершина -> массив коэффициентов при шумах на всех рёбрах"
+    beta = {start: np.zeros(len(orientation.nodes()))}
+    # для каждой вершины...
+    for u in topsort:
+        beta[u] = np.zeros(len(orientation.nodes()))
+        # перебрать все входящие рёбра
+        for v, _, data in orientation.in_edges(u, data=True):
+            # v - вершина, из которой идёт ребро в u
+            alpha_v = alpha[data['num']]  # коэффициент на ребре (v, u)
+            # рассчитываем коэффициенты в вершине u
+            beta[u] += alpha_v * beta[v]
+            beta[u][data['num']] += 1  # единичная дисперсия всех шумов
+            # TODO: поменять 1 на произвольные числа на рёбрах
+
+    # Вычислить дисперсии во всех вершинах как сумму квадратов коэффициентов...
+    variance = {}  # словарь "вершина -> дисперсия в этой вершине"
+    for v in orientation.nodes():
+        if v in beta:
+            variance[v] = np.sum(beta[v] ** 2)
+        else:
+            variance[v] = np.inf
+
+    return variance
 
 
 def optimize_coefs(orientation, initial_alpha, start, finish):
@@ -97,7 +130,15 @@ def optimize_orientation(graph, start, finish):
 
 
 
+# добавить служебную информацию к рёбрам графа
+# нумерация ребер графа
+edges_list = []
+for i, e in enumerate(G.edges):
+    G.edges[e]['num'] = i
+    edges_list.append(e)
+
+
 orientation0 = get_initial_orientation(G)
 optimal_orientation = optimize_orientation(G, source, destination)
 
-
+print(calc_variance(orientation0, None, 1, 4))
